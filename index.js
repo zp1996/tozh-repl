5aaee8df7e433c7472f82073e9fea056a6093efe
周鹏 <1179258516@qq.com>
2017-11-17 22:12:12 +0800

    添加eslint

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const http = require('http');
 
-// 发起请求
-http.get(
-  'http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&from=en&to=zh&appid=2015063000000001&salt=1435660288&sign=f89f9594663708c1605f3d736d01d2d4',
-  function(res) {
-    const { statusCode } = res;
-    if (statusCode !== 200) {
-      console.log('请求发生错误~')
-    }
-    
-  }
-);
